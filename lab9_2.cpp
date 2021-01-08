4a75d8ae8388c1c7b81352e2787ece51025e3b08
minmeeen <sirapat.mp@gmail.com>
2021-01-08 16:25:00 +0700

    lab9_2edit

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -6,11 +6,31 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+ 
+    string a;
+	getline(source, a);
+	dest <<"-------------------- BOOM ---------------------\n"<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	getline(source, a);
+	dest<<a<<"\n";
+	dest<< "-------------------- HA!! ---------------------";
+
 
     source.close();
     dest.close();
